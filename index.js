dcdfaa5ca4b161160fbc3f63df726a6fbafcbcca
Jasur <saribaev2015@mail.ru>
2020-04-06 11:57:50 +0500

    add new propert

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const numbers = [1, 2, 2];
-const books = { name: "Harry Potter", author: "J.R", year: 2000 };
+const books = { name: "Harry Potter", author: "J.R", year: 2000, price: 1500 };
 console.log(numbers);
